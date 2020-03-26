95c7b1485749f0c63d93d1c0dc30b0a162897ce3
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2020-03-26 22:55:03 +0200

    Chapter 4.1. Element Interactions

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,23 +5,21 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.findElement(By.linkText("Shifting Content")).click();
-        driver.findElement(By.linkText("Example 1: Menu Element")).click();
-        List<WebElement> menuItems = driver.findElements(By.xpath("//div[@id='content']/div/ul//a")); // my version
-        //List<WebElement> menuItems = driver.findElements(By.tagName("li")); // course version
-        System.out.println("Number of menu items: " + menuItems.size());
+        homePage = new HomePage(driver);
 
         driver.quit();
     }
