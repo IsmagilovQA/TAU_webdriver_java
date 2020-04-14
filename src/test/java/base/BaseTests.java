7bde217cbfa97b70f0433eb4b218d163b4ff576b
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2020-04-14 23:47:12 +0300

    Chapter 14. WebDriver Extras (chrome options, cookie)

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,8 +3,8 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -12,6 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.CookieManager;
 import utils.EventReporter;
 import utils.WindowManager;
 
@@ -26,12 +27,13 @@ public class BaseTests {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
         driver.register(new EventReporter());
         //driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
         //driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
+        getCookieManager().setCookie();
 
         homePage = new HomePage(driver);
     }
@@ -57,11 +59,20 @@ public class BaseTests {
                 e.printStackTrace();
             }
         }
-
-
     }
 
     public WindowManager getWindowManager() {
         return new WindowManager(driver);
+    }
+
+    public CookieManager getCookieManager(){
+        return new CookieManager(driver);
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        //options.setHeadless(true);
+        return options;
     }
 }
