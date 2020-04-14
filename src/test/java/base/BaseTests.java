d592066a536c458bbd31faf42fbb5c71feac74de
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2020-04-14 22:33:07 +0300

    Chapter 12. Taking Screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,14 +1,20 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
 
@@ -27,17 +33,29 @@ public class BaseTests {
         homePage = new HomePage(driver);
     }
 
-
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-
-
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            //System.out.println("Screenshot is taken: " + screenshotFile.getAbsolutePath());
+            try {
+                Files.move(screenshotFile, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
     public WindowManager getWindowManager() {
