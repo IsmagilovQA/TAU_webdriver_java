1989912107c6ebab31f0583d53ea28c3592ed6c4
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2020-04-14 19:00:27 +0300

    Chapter 11. Application Navigation (task - switch to new tab/window)

---
+++ src/test/java/navigation/NavigationTests.java
@@ -0,0 +1,34 @@
+package navigation;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.DynamicLoadingExample2Page;
+import pages.DynamicLoadingPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class NavigationTests extends BaseTests {
+
+    @Test
+    public void testNavigator() {
+        homePage.clickDynamicLoading().clickExample1();
+        getWindowManager().goBack();
+        getWindowManager().refreshPage();
+        getWindowManager().goForward();
+        getWindowManager().goTo("https://google.com");
+    }
+
+    @Test
+    public void testSwitchTab() {
+        homePage.clickMultipleWindows().clickHere();
+        getWindowManager().switchToTab("New Window");
+    }
+
+    @Test
+    public void testWindowTabs(){
+        DynamicLoadingExample2Page buttonPage = homePage.clickDynamicLoading().rightClickOnExample2Link();
+        getWindowManager().switchToNewTab();
+        assertTrue(buttonPage.isStartButtonDisplayed(), "Start button is not displayed");
+    }
+}
+
